befb24ec6e150ea3b758f520b5f6802eafc31120
andreakruining <afk05@live.nl>
2023-03-27 12:15:19 +0200

    hulp bij docent

--- pbw 2 unity/Assets/01. scripts/DoorController.cs
+++ pbw 2 unity/Assets/01. scripts/DoorController.cs
@@ -6,17 +6,25 @@ public class DoorController : MonoBehaviour
 {
     [SerializeField] private Animator Door = null;
 
+    private GameObject fence;
+
     // [SerializeField] private bool openTrigger = false;
     // [SerializeField] private bool closeTrigger = false;
 
+    void Start()
+    {
+        fenceCollider = fence.GetComponent<BoxCollider>();
+        fenceCollider = true; 
+    }
+
     private void OnTriggerEnter(Collider openen)
     {
-
         if(openen.GetComponent<Collider>().tag == "Egg")
         {
             Debug.Log("open");
-            
             Door.SetTrigger("OpenTrigger");
+            fenceCollider = false;
+
         }
     }
 
--- pbw 2 unity/Assets/01. scripts/JumpMove.cs
+++ pbw 2 unity/Assets/01. scripts/JumpMove.cs
@@ -8,6 +8,9 @@ public class JumpMove : MonoBehaviour
     public float JumpForce = 5f;
 
     public GameObject egga;
+    private float horizontal;
+    private float vertical;
+    public Rigidbody rb;
 
     void Start()
     {
@@ -20,25 +23,29 @@ public class JumpMove : MonoBehaviour
         Move();
     }
 
+    void FixedUpdate()
+    {
+        rb.MovePosition(transform.position + horizontal * Speed * Time.deltaTime);
+        rb.MovePosition(transform.position + vertical * Speed * Time.deltaTime);
+    }
+
     public void Move()
     {
         Vector3 position = transform.position;
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
 
-        if(Input.GetKey("a"))
-        {
-            position.x -= Speed * Time.deltaTime;
-            transform.Rotate(Vector3.up * Speed * Time.deltaTime);
-        }
+        // if(Input.GetKey("a"))
+        // {
+        //     position.x -= Speed * Time.deltaTime;
+        //     transform.Rotate(Vector3.up * Speed * Time.deltaTime);
+        // }
 
-        transform.position = position;
-        
-        if(Input.GetKey("d"))
-        {
-            position.x += Speed * Time.deltaTime;
-            transform.Rotate(-Vector3.up * Speed * Time.deltaTime);
-        }
-
-        transform.position = position;
+        // if(Input.GetKey("d"))
+        // {
+        //     position.x += Speed * Time.deltaTime;
+        //     transform.Rotate(-Vector3.up * Speed * Time.deltaTime);
+        // }
 
         if(Input.GetKey("space"))
         {
@@ -57,8 +64,5 @@ public class JumpMove : MonoBehaviour
             //egga.gameObject.transform.localScale = new Vector3(1, 0, 1);
             JumpForce += 5f;
         }
-
-        JumpForce = 5f;
-
     }
 }
