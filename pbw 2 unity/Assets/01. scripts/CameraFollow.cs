3abd4bca0df23d0a4c129bd79ad5fde0a97fa657
andreakruining <afk05@live.nl>
2023-05-22 17:28:42 +0200

    updating

--- pbw 2 unity/Assets/01. scripts/CameraFollow.cs
+++ pbw 2 unity/Assets/01. scripts/CameraFollow.cs
@@ -4,12 +4,48 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    [SerializeField]
+    GameObject player;
 
-    public Transform player;
-    public Vector3 offset;
-    // Update is called once per frame
-    void Update()
+    [SerializeField]
+    float timeOffset;
+
+    [SerializeField]
+    Vector3 posOffset;
+
+    private Vector3 veloc;
+
+    private void Update()
     {
-        transform.position = player.position + offset;
+        //cameras current position
+        Vector3 startPos = transform.position;
+        //players current position
+        Vector3 endPos = player.transform.position;
+
+        endPos.x += posOffset.x;
+        endPos.y += posOffset.y;
+        endPos.z = -10;
+
+        //smoothly move camera towards player pos
+        transform.position = Vector3.Lerp(startPos, endPos, timeOffset * Time.deltaTime);
+
     }
+    //public Transform player;
+    //public Vector3 offset;
+    //// Update is called once per frame
+
+    //private void Start()
+    //{
+    //    offset = transform.position = new Vector3(
+    //        Mathf.Clamp(player.position.x, 1.077981f, 1.22f),
+    //        Mathf.Clamp(player.position.y, 0.5530509f, 0.56f),
+    //        transform.position.z);
+
+    //    transform.position = offset;
+    //}
+    //void Update()
+    //{
+    //    transform.position = player.position + offset;
+
+    //}
 }
