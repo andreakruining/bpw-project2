af4176ec73f83908cbe83bb0d127aef43d716da1
andreakruining <afk05@live.nl>
2023-05-22 13:56:37 +0200

    call egg and throw egg

--- pbw 2 unity/Assets/01. scripts/CallEgg.cs
+++ pbw 2 unity/Assets/01. scripts/CallEgg.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class CallEgg : MonoBehaviour
 {
-    public float callSpeed= 5f;
+    public float callSpeed = 5f;
     public Rigidbody rb;
-    public GameObject player;
+    public Transform player;
 
     void Update()
     {
         if(Input.GetKey("c"))
         {
-            rb.velocity = Vector3.zero; 
+            Vector3 actualTarget = new Vector3(transform.position.x, player.transform.position.y, transform.position.z);
+            //rb.velocity = Vector3.zero; 
 
             transform.position = Vector3.MoveTowards(transform.position, player.transform.position, callSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("v"))
+        {
+            //Vector3 actualTarget = new Vector3(transform.position.x, transform.position.y, player.transform.position.z);
+            rb.velocity = transform.right * 10;
+            
+            //transform.position = Vector3.MoveTowards(transform.position, -player.transform.position, callSpeed * Time.deltaTime);
         }
     }
 }
