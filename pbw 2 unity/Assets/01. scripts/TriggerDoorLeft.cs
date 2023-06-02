bee3d142224ef13db9bebd5574bc72c9eea79657
andreakruining <afk05@live.nl>
2023-06-02 15:25:40 +0200

    some scripting

--- pbw 2 unity/Assets/01. scripts/TriggerDoorLeft.cs
+++ pbw 2 unity/Assets/01. scripts/TriggerDoorLeft.cs
@@ -6,11 +6,14 @@ public class TriggerDoorLeft : MonoBehaviour
 {
     [SerializeField] GameObject door;
 
+    bool isOpened = false;
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if(other.CompareTag("Player") && !isOpened)
         {
             Debug.Log("chick in place");
+            isOpened = true;
             door.transform.position += new Vector3(1, 0, 0);
         }
     }
--- pbw 2 unity/Assets/01. scripts/TriggerDoorRight.cs
+++ pbw 2 unity/Assets/01. scripts/TriggerDoorRight.cs
@@ -6,11 +6,14 @@ public class TriggerDoorRight : MonoBehaviour
 {
     [SerializeField] GameObject triggerBlock;
 
+    bool isOpened = false;
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Egg"))
+        if (other.CompareTag("Egg") && !isOpened)
         {
             Debug.Log("egg in place");
+            isOpened = true;
             triggerBlock.transform.position += new Vector3(1, 0, 0);
         }
     }
